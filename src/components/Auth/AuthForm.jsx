72dc59b7fdb0135fef63c31a19500ef0ee29a502
danonishe <danissimo1323@mail.ru>
2024-04-21 13:49:59 +0300

    late 3

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MyInput from "../UI/Input/Input";
 import MyButton from "../UI/Button/Button";
 import { AuthContext } from "../../context";
